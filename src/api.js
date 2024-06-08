da646587c852c709406b55bf77178c7950c09572
ssherliann <popova.ann05@gmail.com>
2024-06-08 19:48:15 +0300

    upd

--- src/api.js
+++ src/api.js
@@ -102,21 +102,17 @@ export const postOrder = async (input) => {
       input,
       {
         headers: {
-          Authorization: `Bearer ${token}`, // Ensure the 'Bearer' prefix is used
+          Authorization: `Bearer ${token}`, 
         },
       }
     );
     return data;
   } catch (e) {
     if (e.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
       console.error("Server responded with an error:", e.response.data);
     } else if (e.request) {
-      // The request was made but no response was received
       console.error("No response received:", e.request);
     } else {
-      // Something happened in setting up the request that triggered an Error
       console.error("Error setting up the request:", e.message);
     }
   }
